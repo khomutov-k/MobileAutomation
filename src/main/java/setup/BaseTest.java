29da5804d4aac31f2d240cfe9d993c6d026ffb5d
Konstantin Khomutov <konstantin_khomutov@epam.com>
2020-09-28 16:59:39 +0300

    Issues were fixed corresponding notes.

--- src/main/java/setup/BaseTest.java
+++ src/main/java/setup/BaseTest.java
@@ -19,7 +19,7 @@ import java.util.concurrent.TimeUnit;
 public class BaseTest {
 
     static String resourceFilePath = "./src/test/resources/";
-    private AppiumDriver appiumDriver; // singleton
+    private AppiumDriver appiumDriver;
 
     public AppiumDriver getDriver() {
         return appiumDriver;
@@ -28,15 +28,15 @@ public class BaseTest {
     @Parameters({"platformName", "appType", "deviceName", "udid", "osVersion","resourceFilePath", "browserName",
             "appPackage", "appActivity", "bundleId"})
     @BeforeSuite(alwaysRun = true)
-    public void setUp(String platformName, String appType, @Optional("") String deviceName,String udid,
-                      @Optional("9.0") String osVersion,
+    public void setUp(String platformName, String appType, @Optional("") String deviceName,
+                      @Optional("") String udid,
                       @Optional("src/test/resources/") String resourceFilePath,
                       @Optional("") String browserName,
                       @Optional("") String appPackage, @Optional("") String appActivity,
                       @Optional("") String bundleId) throws Exception {
         System.out.println("Before: app type - " + appType);
         BaseTest.resourceFilePath = resourceFilePath;
-        setAppiumDriver(appType,platformName,udid, osVersion, deviceName,
+        setAppiumDriver(appType,platformName,udid,
                 browserName, appPackage,appActivity,bundleId);
     }
 
@@ -51,20 +51,15 @@ public class BaseTest {
         }
     }
 
-    private void setAppiumDriver(String appType,String platformName,String udid, String osVersion, String deviceName,
+    private void setAppiumDriver(String appType,String platformName,String udid,
                                  String browserName, String appPackage, String appActivity, String bundleId) {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         //mandatory Android capabilities
         capabilities.setCapability("platformName", platformName);
-//        capabilities.setCapability("device", deviceName);
         capabilities.setCapability("udid", udid);
-        capabilities.setCapability("os_version", osVersion);
-//        capabilities.setCapability("project", "EpamTA Project");
-//        capabilities.setCapability("build", "My First Build");
-//        capabilities.setCapability("name", appType + " tests launch");
         if (appType.equalsIgnoreCase("web")) {
             capabilities.setCapability("browserName", browserName);
-//            capabilities.setCapability("chromedriverDisableBuildCheck", "true");
+            capabilities.setCapability("chromedriverDisableBuildCheck", "true");
             capabilities.setCapability("autoAcceptAlerts", true);
         } else {
             capabilities.setCapability("bundleId",bundleId);
